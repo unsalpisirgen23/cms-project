2c296d4743a93659da5f1c0c2c6cb1894c443fdf
unsal pisirgen <unsalpisirgen23@hotmail.com>
2022-01-30 21:52:30 +0300

    slm

---
+++ database/file.sql
@@ -0,0 +1,8 @@
+DELIMITER //
+CREATE TRIGGER post_to_delete BEFORE DELETE
+    ON posts
+    FOR EACH ROW
+BEGIN
+    delete from post_categories where post_categories.post_id = OLD.id;
+END//
+DELIMITER ;
